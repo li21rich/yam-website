adf22bbb6a555bee546f5e7a25792820c9df2c05
Richard Li <lilrichardh@gmail.com>
2024-04-13 23:31:39 -0700

    Updates

---
+++ precache-manifest.675e68ecf2c08a9d2b901001bb396d16.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53325dcd45d3f43546fa60332cab7bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0a05a3e3e1ebb8981fc",
+    "url": "./static/css/main.b2958208.chunk.css"
+  },
+  {
+    "revision": "8e3818130d3836ec09fc",
+    "url": "./static/js/2.9ed6ffd5.chunk.js"
+  },
+  {
+    "revision": "b38e18c352034ed146b22292e6eb4495",
+    "url": "./static/js/2.9ed6ffd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a05a3e3e1ebb8981fc",
+    "url": "./static/js/main.b10bc28b.chunk.js"
+  },
+  {
+    "revision": "7d9ccdf9468f58d86414",
+    "url": "./static/js/runtime-main.6460e0c0.js"
+  },
+  {
+    "revision": "adccdc3d9ce00ae37f1ce55899538fa8",
+    "url": "./static/media/GregBrown.adccdc3d.png"
+  },
+  {
+    "revision": "d95528e2c848ffd06a42ae2c074e3c4b",
+    "url": "./static/media/JeffMartin.d95528e2.png"
+  },
+  {
+    "revision": "ff8be4585358a09844d1238dffa2b0d8",
+    "url": "./static/media/JohnCaserta.ff8be458.png"
+  },
+  {
+    "revision": "b706a75896daf2cd27ac5c95d0fb615b",
+    "url": "./static/media/MarieOtsuka.b706a758.png"
+  },
+  {
+    "revision": "c37484cf40e00f446d64dc21dc6cdc24",
+    "url": "./static/media/MauraKelly.c37484cf.png"
+  },
+  {
+    "revision": "721a02d67a48290f2883588302bd9471",
+    "url": "./static/media/RyanWilliams.721a02d6.png"
+  },
+  {
+    "revision": "7f78c8b5ba68018bc4681ac9abe407e2",
+    "url": "./static/media/YvetteChaparro.7f78c8b5.png"
+  },
+  {
+    "revision": "709ad5c81602dd3bf51df74155f6d9b9",
+    "url": "./static/media/ama.709ad5c8.png"
+  },
+  {
+    "revision": "412eed2399d026eb6dfa65feebb4f409",
+    "url": "./static/media/arthublogo.412eed23.png"
+  },
+  {
+    "revision": "29c8916f73e6eaee767e9730a337043b",
+    "url": "./static/media/financeoutreach.29c8916f.PNG"
+  },
+  {
+    "revision": "839212858eac16a50194636d319c6dd4",
+    "url": "./static/media/graphicsoutreach.83921285.PNG"
+  },
+  {
+    "revision": "263e19c1087a945f6f6c20a20931723d",
+    "url": "./static/media/happyart.263e19c1.png"
+  },
+  {
+    "revision": "0916b0abff9b20727186bb87afbb684e",
+    "url": "./static/media/key2music.0916b0ab.png"
+  },
+  {
+    "revision": "b79b1987099f684aff7ea8d66804d3d8",
+    "url": "./static/media/lapluma.b79b1987.png"
+  },
+  {
+    "revision": "8283235e1ccdd5f030155ebd67c9e3f6",
+    "url": "./static/media/lg-color.8283235e.jpg"
+  },
+  {
+    "revision": "ed6dca615bae63b4db5169f261ac6ae7",
+    "url": "./static/media/lovenote.ed6dca61.png"
+  },
+  {
+    "revision": "a7d26278b1f09a75a6cf453ac989f51d",
+    "url": "./static/media/marketingoutreach.a7d26278.JPG"
+  },
+  {
+    "revision": "6a77ad34635710076509eb0067f8128b",
+    "url": "./static/media/pic1.6a77ad34.png"
+  },
+  {
+    "revision": "88c636e39955108aec7491dbdf298fef",
+    "url": "./static/media/pic2.88c636e3.png"
+  },
+  {
+    "revision": "8bec2b0c0eaa80e6692b53ff20c19967",
+    "url": "./static/media/pic3.8bec2b0c.png"
+  },
+  {
+    "revision": "82be0e1c26bf8b055a0c0fd372245fdc",
+    "url": "./static/media/pic4.82be0e1c.png"
+  },
+  {
+    "revision": "aafcb5d00dd6892eacd86b3062041c5f",
+    "url": "./static/media/secretaryoutreach.aafcb5d0.PNG"
+  },
+  {
+    "revision": "441fb9f1248a7d28ceac88b7961fa0e8",
+    "url": "./static/media/techoutreach.441fb9f1.PNG"
+  },
+  {
+    "revision": "9fc6419fdb0b69333e4ead9ee6047068",
+    "url": "./static/media/theDrawingStudio.9fc6419f.png"
+  },
+  {
+    "revision": "9a7167978b65e9c38d4b78dacc59c7e3",
+    "url": "./static/media/visvalogo.9a716797.svg"
+  },
+  {
+    "revision": "6d4fb1891b42605de6e30de8993026a3",
+    "url": "./static/media/yamLogo.6d4fb189.png"
+  }
+]);
