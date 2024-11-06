e899ec0d5f630b94a3d951c9c4a8e6cdd4e61c8e
reykophi <reykophi@gmail.com>
2024-11-05 23:30:22 -0800

    Added In a Room Alone in writing gallery

--- src/data/galleries.js
+++ src/data/galleries.js
@@ -769,6 +769,27 @@ export const blm = [
 ];
 
 export const creativewriting = [
+{
+    imageURL:
+      "https://res.cloudinary.com/dji4rmhi5/image/upload/v1730877704/Untitled_document_-_Alanna_Tu_page-0001_uys3sr.jpg",
+    artist: "Anonymous",
+        title: "In a Room Alone",
+    description: "The artist has not provided a description for this piece.",
+    writing: (
+      <div className="container p-0 m-0">
+        <div className="row no-gutters">
+          <div className="col">
+            <img
+              src="https://res.cloudinary.com/dji4rmhi5/image/upload/v1730877704/Untitled_document_-_Alanna_Tu_page-0001_uys3sr.jpg"
+              alt="In a Room Alone by Anonymous"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    ),
+  },
+
  {
     imageURL:
       "https://res.cloudinary.com/dji4rmhi5/image/upload/v1730877085/Morning-Glory-Nellie-Rathod-Banuelos_fqbczj.jpg",
