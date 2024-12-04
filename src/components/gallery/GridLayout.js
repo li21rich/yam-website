b00397263ed76ffa648bbf879ceaa85c9bb5151a
Richard Li <lilrichardh@gmail.com>
2024-12-03 21:54:39 -0800

    changed badge positioning

--- src/components/gallery/GridLayout.js
+++ src/components/gallery/GridLayout.js
@@ -5,15 +5,13 @@ import { Link } from "react-router-dom";
 const WinnerBadge = ({ award }) => {
   const { color } = award;
   return (
-    <div className="absolute top-2 left-2 bg-yellow-500 text-black rounded-full text-sm font-semibold shadow-md z-10 flex items-center justify-center w-8 h-8">
       <svg
-        className="w-6 h-6"
+        className="absolute top-2 left-2 bg-yellow-500 text-black rounded-full text-sm font-semibold shadow-md z-10 flex items-center justify-center w-8 h-8"
         viewBox="0 0 20 20"
         fill={color}
       >
         <path d="M 3.3933 1.0977 c 0.1125 -0.3453 0.6012 -0.3453 0.7131 0 l 0.4014 1.2345 a 0.375 0.375 90 0 0 0.3561 0.2586 h 1.2984 c 0.3633 0 0.5142 0.465 0.2205 0.6789 l -1.05 0.7626 a 0.375 0.375 90 0 0 -0.1365 0.4194 l 0.4014 1.2345 c 0.1125 0.3453 -0.2832 0.633 -0.5775 0.4194 l -1.05 -0.7626 a 0.375 0.375 90 0 0 -0.4407 0 l -1.05 0.7626 c -0.294 0.2136 -0.6894 -0.0738 -0.5772 -0.4194 l 0.4014 -1.2345 a 0.375 0.375 90 0 0 -0.1365 -0.4194 L 1.1175 3.27 c -0.2937 -0.2136 -0.1425 -0.6789 0.2205 -0.6789 h 1.2978 a 0.375 0.375 90 0 0 0.3567 -0.2586 l 0.4014 -1.2345 z" />
       </svg>
-    </div>
   );
 };
 
